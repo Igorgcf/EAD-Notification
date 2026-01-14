7eaf2d9c57d03306d59b6b9e7020f8312123629b
Igorgcf <igorTecnologia@outlook.com>
2026-01-14 10:51:16 -0300

    Notification Repository

---
+++ EAD-Notification/src/main/java/com/ead/notification/repositories/NotificationRepository.java
@@ -0,0 +1,18 @@
+package com.ead.notification.repositories;
+
+import com.ead.notification.enums.NotificationStatus;
+import com.ead.notification.models.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+    Page<Notification> findAllPagedByUserIdAndNotificationStatus(UUID userId, NotificationStatus notificationStatus, Pageable pageable);
+
+    Optional<Notification> findByIdAndUserId(UUID id, UUID userId);
+
+}
